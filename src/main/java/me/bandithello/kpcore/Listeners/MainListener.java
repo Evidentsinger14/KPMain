6077f404994e1221971fc218e3a38c06cf9c81da
Evidentsinger14 <cdelancey186423@gmail.com>
2024-07-14 15:52:57 -0400

    remove unused imports

--- src/main/java/me/bandithello/kpcore/Listeners/MainListener.java
+++ src/main/java/me/bandithello/kpcore/Listeners/MainListener.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.newplugin.kpmain.GUI.*;
 
 public class MainListener implements Listener {
     @EventHandler
